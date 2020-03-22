a3611d610005484083e35c9599fb0a4a85a6ea78
xiedong <xiedong@rmitec.cn>
2020-03-22 21:57:29 +0800

    consumer注册至Eureka

--- cloud-consumer-order80/src/main/java/com/xd/springcloud/Ordermain80.java
+++ cloud-consumer-order80/src/main/java/com/xd/springcloud/Ordermain80.java
@@ -2,6 +2,7 @@ package com.xd.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @ClassName: Ordermain80
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Date: 2020/3/22 17:00
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class Ordermain80 {
      public static void main(String[] args) {
              SpringApplication.run(Ordermain80.class,args);
