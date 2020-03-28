9b6e454667ac4e97e60e55ae77437f2425e62e07
xiedong <xiedong@rmitec.cn>
2020-03-28 16:33:41 +0800

    Stream消费者消费消息成功

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/xd/springcloud/StreamMQMain8802.java
@@ -0,0 +1,17 @@
+package com.xd.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName: StreamMQMain8802
+ * @Description:
+ * @Author: xiedong
+ * @Date: 2020/3/28 16:22
+ */
+@SpringBootApplication
+public class StreamMQMain8802 {
+     public static void main(String[] args) {
+             SpringApplication.run(StreamMQMain8802.class,args);
+         }
+}
