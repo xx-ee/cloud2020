b994c5e722a2141ed47f4cd1b95b50a3c003d2a0
xiedong <xiedong@rmitec.cn>
2020-03-28 22:55:09 +0800

    Nacos配置中心基本配置

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/xd/springcloud/NacosConfigClientMain3377.java
@@ -0,0 +1,20 @@
+package com.xd.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @ClassName: NacosConfigClientMain3377
+ * @Description:
+ * @Author: xiedong
+ * @Date: 2020/3/28 21:35
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosConfigClientMain3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class, args);
+    }
+}
+
