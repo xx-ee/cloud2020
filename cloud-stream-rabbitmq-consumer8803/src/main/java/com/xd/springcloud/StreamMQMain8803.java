d93c8e251a0fcb4ae97c3182ef49412e58aa28b3
xiedong <xiedong@rmitec.cn>
2020-03-28 17:38:19 +0800

    Stream消费者组配置

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/xd/springcloud/StreamMQMain8803.java
@@ -0,0 +1,17 @@
+package com.xd.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName: StreamMQMain8803
+ * @Description:
+ * @Author: xiedong
+ * @Date: 2020/3/28 16:36
+ */
+@SpringBootApplication
+public class StreamMQMain8803 {
+     public static void main(String[] args) {
+             SpringApplication.run(StreamMQMain8803.class,args);
+         }
+}
---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/xd/springcloud/controller/ReceiveMessageListenerController.java
@@ -0,0 +1,26 @@
+package com.xd.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+/**
+ * @ClassName: ReceiveMessageListenerController
+ * @Description:
+ * @Author: xiedong
+ * @Date: 2020/3/28 16:36
+ */
+@Component
+@EnableBinding(Sink.class)
+public class ReceiveMessageListenerController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @StreamListener(Sink.INPUT)
+    public void input(Message<String> message){
+        System.out.println("消费者2号，----->接收到的消息："+ message.getPayload() +"\t port:" + serverPort);
+    }
+}
