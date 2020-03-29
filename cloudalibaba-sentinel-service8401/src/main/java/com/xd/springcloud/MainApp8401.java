ae1a005f9b1827fae23bea56233356827cd62361
xiedong <xiedong@rmitec.cn>
2020-03-29 18:35:59 +0800

    Sentinel，服务懒加载

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/xd/springcloud/MainApp8401.java
@@ -0,0 +1,19 @@
+package com.xd.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @ClassName: MainApp8401
+ * @Description:
+ * @Author: xiedong
+ * @Date: 2020/3/29 17:44
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MainApp8401 {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApp8401.class, args);
+    }
+}
